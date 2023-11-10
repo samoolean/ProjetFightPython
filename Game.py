a23f6b5359b5ed33b9203ef0c4fa85df7ab1063d
samoolean <samuel.choisi1@gmail.com>
2023-11-10 14:51:35 +0100

    message for changes

---
+++ Game.py
@@ -0,0 +1,29 @@
+import pygame
+
+#creation de l'ecran d'acceuil
+
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+scrrec = window.get_rect()
+
+class game:
+
+    def run(self):
+
+            clock = pygame.time.Clock()
+
+            #boucle du jeux
+
+            running = True
+
+            while running:
+
+                pygame.display.flip()
+
+            
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+
+                clock.tick(60)
+
+            pygame.quit()
---
+++ Main.py
@@ -0,0 +1,11 @@
+# ()
+# 5
+#
+
+import pygame
+from Game import game
+
+if __name__ == '__main__':
+    pygame.init()
+    game = game()
+    game.run()
