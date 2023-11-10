db7647e1d8bba65bbc148bdd9f603901c69efba5
samoolean <samuel.choisi1@gmail.com>
2023-11-10 15:30:42 +0100

    a

--- Class/player.py
+++ Class/player.py
@@ -11,7 +11,8 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.sprite_sheet = pygame.image.load('player.png')
+        self.sprite_sheet_idle = pygame.image.load('asset\\red hood\\idle sheet-Sheet.png')
 
     def get_image(self, x, y):
         image = pygame.Surface([32, 32])
+        image.blit
